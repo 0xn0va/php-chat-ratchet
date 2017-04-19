2a127fc30625567869ad3f08c9fa0c79d11b173f
Unknown <ditrich13@gmail.com>
2017-04-19 12:22:18 +0300

    Login/Sign-up draft

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 
 session_start();
@@ -18,54 +18,104 @@ if (isset($_POST['btn-login'])) {
         $user_login->redirect('dashboard.php');
     }
 }
-?>
+?> -->
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
-    <link href="resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="resources/css/style.css" rel="stylesheet" media="screen">
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link href="resources/css/style.css" rel="stylesheet" media="screen">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+      <style type="text/css">
+
+          body {
+            background-image: url("resources/1.png");
+            background-size: cover;
+            font-family: "Calibri", "Roboto", sans-serif;
+        }
+
+        .form-signin-heading {
+            font-style: bold;
+            font-weight: 500;
+            color: #edeff2;
+        }
+
+        a {
+            color: #edeff2;
+        }
+
+         .btn:hover {
+            color: #de2a3d;
+        }
+
+        .btn-primary {
+            background-color: #edeff2;
+            color: #de2a3d;
+        }
+
+        .btn-primary:hover {
+            color: #edeff2;
+             background-color: #de2a3d;
+            border-color: #edeff2;
+        }
+
+       
+
+        .form-signin .form-control {
+            color: #de2a3d;
+            font-size: 18px;
+        }
+
+    </style>
 </head>
 <body id="login">
-<div class="container">
-    <?php
-    if (isset($_GET['inactive'])) {
-        ?>
-        <div class='alert alert-error'>
-            <button class='close' data-dismiss='alert'>&times;</button>
-            <strong> Please activate your account, you can find activation email in your inbox.</strong>
-        </div>
+    <div class="container">
         <?php
-    }
-    ?>
-    <form class="form-signin" method="post">
-        <?php
-        if (isset($_GET['error'])) {
+        if (isset($_GET['inactive'])) {
             ?>
-            <div class='alert alert-success'>
+            <div class='alert alert-error'>
                 <button class='close' data-dismiss='alert'>&times;</button>
-                <strong> Oops... Something is wrong here!</strong>
+                <strong> Please activate your account, you can find activation email in your inbox.</strong>
             </div>
             <?php
         }
         ?>
+        <form class="form-signin" method="post">
+            <?php
+            if (isset($_GET['error'])) {
+                ?>
+                <div class='alert alert-success'>
+                    <button class='close' data-dismiss='alert'>&times;</button>
+                    <strong> Oops... Something is wrong here!</strong>
+                </div>
+                <?php
+            }
+            ?>
 
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" class="form-control" placeholder="Email address" name="input_email" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" class="form-control" placeholder="Password" name="input_password" required>
+            <h2 class="form-signin-heading">Please sign in</h2>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" class="form-control" placeholder="Email address" name="input_email" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" class="form-control" placeholder="Password" name="input_password" required>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-login">Login</button>
-        <a href="subscribe.php" style="float:right;" class="btn btn-large">Subscribe</a>
-    </form>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-login">Login</button>
+            <a href="subscribe.php" style="float:right;" class="btn btn-large">Subscribe</a>
+        </form>
 
-</div>
-<script src="resources/bootstrap/js/bootstrap.min.js"></script>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="chat.js"></script>
 </body>
 </html>
--- subscribe.php
+++ subscribe.php
@@ -27,19 +27,19 @@ if (isset($_POST['btn-signup'])) {
         $domains = array("hotmail.com", "yahoo.com");
         if (in_array($inputdomain, $domains)) {
             $msg = "
-          <div class='alert alert-error'>
-        <button class='close' data-dismiss='alert'>&times;</button>
-                    Ouch, sorry for this but you can only login with corporate email address
-        </div>
-        ";
+            <div class='alert alert-error'>
+                <button class='close' data-dismiss='alert'>&times;</button>
+                Ouch, sorry for this but you can only login with corporate email address
+            </div>
+            ";
         } else {
             if ($result->rowCount() > 0) {
                 $msg = "
-          <div class='alert alert-error'>
-        <button class='close' data-dismiss='alert'>&times;</button>
-           Your data is with us, don't register again, please login instead!
-        </div>
-        ";
+                <div class='alert alert-error'>
+                    <button class='close' data-dismiss='alert'>&times;</button>
+                    Your data is with us, don't register again, please login instead!
+                </div>
+                ";
             } else {
                 if ($new_msg->form($username, $email, $password, $salt, $verification)) {
                     $id = $new_msg->lastID();
@@ -47,22 +47,22 @@ if (isset($_POST['btn-signup'])) {
                     $id = $key;
 
                     $message = "
-            Hey $username,
-            <br />
-            You subscribed!<br/>
-            You just need to click on the following link to verify your email<br/>
-            <a href='http://localhost/liana/abdo/verification.php?id=$id&code=$verification'>CLICK ON ME!</a><br />
-            Best Regards,";
+                    Hey $username,
+                    <br />
+                    You subscribed!<br/>
+                    You just need to click on the following link to verify your email<br/>
+                    <a href='http://localhost/liana/abdo/verification.php?id=$id&code=$verification'>CLICK ON ME!</a><br />
+                    Best Regards,";
 
                     $subject = "Verify Subscription";
 
                     $new_msg->send_mail($email, $message, $subject);
                     $msg = "
-          <div class='alert alert-success'>
-            <button class='close' data-dismiss='alert'>&times;</button>
-            <strong>Thats it!, we just sent an email to the address you just entered, please verify your email</strong>
-            </div>
-          ";
+                    <div class='alert alert-success'>
+                        <button class='close' data-dismiss='alert'>&times;</button>
+                        <strong>Thats it!, we just sent an email to the address you just entered, please verify your email</strong>
+                    </div>
+                    ";
                 } else {
                     echo "Ouch, something is wrong! :(";
                 }
@@ -74,32 +74,79 @@ if (isset($_POST['btn-signup'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Subscribe</title>
-    <link href="resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="resources/css/style.css" rel="stylesheet" media="screen">
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Subscribe</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="resources/css/style.css" rel="stylesheet" media="screen">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+      <style type="text/css">
+
+       body {
+            background-image: url("resources/1.png");
+            background-size: cover;
+            font-family: "Calibri", "Roboto", sans-serif;
+        }
+
+        .form-signin-heading {
+            font-style: bold;
+            font-weight: 500;
+            color: #edeff2;
+        }
+
+        a {
+            color: #edeff2;
+        }
+
+         .btn:hover {
+            color: #de2a3d;
+        }
+
+        .btn-primary {
+            background-color: #edeff2;
+            color: #de2a3d;
+        }
+
+        .btn-primary:hover {
+            color: #edeff2;
+             background-color: #de2a3d;
+            border-color: #edeff2;
+        }
+
+       
+
+        .form-signin .form-control {
+            color: #de2a3d;
+            font-size: 18px;
+        }
+
+    </style>
 </head>
 <body id="login">
-<div class="container">
-    <?php if (isset($msg)) echo $msg; ?>
-    <form class="form-signin" method="post">
+    <div class="container">
+        <?php if (isset($msg)) echo $msg; ?>
+        <form class="form-signin" method="post">
 
-        <h2 class="form-signin-heading">Subscribe</h2>
-        <label for="inputName" class="sr-only">Name</label>
-        <input type="text" class="form-control" placeholder="Name" name="input_name" required autofocus>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" class="form-control" placeholder="Email address" name="input_email" required>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" class="form-control" placeholder="Password" name="input_password" required>
+            <h2 class="form-signin-heading">Subscribe</h2>
+            <label for="inputName" class="sr-only">Name</label>
+            <input type="text" class="form-control" placeholder="Name" name="input_name" required autofocus>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" class="form-control" placeholder="Email address" name="input_email" required>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" class="form-control" placeholder="Password" name="input_password" required>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-signup">Subscribe</button>
-        <a href="index.php" style="float:right;" class="btn btn-large">Login</a>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-signup">Subscribe</button>
+            <a href="index.php" style="float:right;" class="btn btn-large">Login</a>
 
-    </form>
-</div>
-<script src="resources/bootstrap/js/bootstrap.min.js"></script>
+        </form>
+    </div>
+    <script src="resources/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
